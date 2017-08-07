1197ca235a0f8d7c423ccc218030a12f37ab5350
slonejk <30811360+slonejk@users.noreply.github.com>
2017-08-07 18:17:42 -0400

    Another Change

--- Module2DisplayingText/Module2DisplayingText.py
+++ Module2DisplayingText/Module2DisplayingText.py
@@ -11,3 +11,5 @@ print('or you can just do this \' does that work')
 print('can I just print a \ on the screen?')
 #I'm inserting a \\ so the \ appears correctly in the string
 print('what if I want \\news?')
+
+print('There once was a movie star icon \nwho preferred to sleep with the light on. \nThey learned how to code \na device that sure glowed \nand it lit up the night using Python')
