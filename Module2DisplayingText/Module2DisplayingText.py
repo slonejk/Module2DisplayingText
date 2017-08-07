900403004e280bca2c2ee6372b60b4f7094f7077
slonejk <30811360+slonejk@users.noreply.github.com>
2017-08-07 18:03:57 -0400

    Committing Changes

--- Module2DisplayingText/Module2DisplayingText.py
+++ Module2DisplayingText/Module2DisplayingText.py
@@ -1,3 +1,12 @@
 print('The capybara is the worlds largest rodents')
 print('The capybara likes to live in groups')
 print("The capybara can swim")
+
+print('The capybara lives in \nSouth America')
+print('''This is the strangest
+way to print over multiple lines I know''')
+
+print('here is a double quote "' + " here is a single quote '")
+print('or you can just do this \' does that work')
+print('can I just print a \ on the screet?')
+print('what if I want \\news?')
