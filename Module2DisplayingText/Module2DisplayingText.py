5aa58406fadcc9a50bb9f07d150d99856a6f6265
slonejk <30811360+slonejk@users.noreply.github.com>
2017-08-07 18:08:00 -0400

    Another Change

--- Module2DisplayingText/Module2DisplayingText.py
+++ Module2DisplayingText/Module2DisplayingText.py
@@ -8,6 +8,6 @@ way to print over multiple lines I know''')
 
 print('here is a double quote "' + " here is a single quote '")
 print('or you can just do this \' does that work')
-print('can I just print a \ on the screet?')
+print('can I just print a \ on the screen?')
 #I'm inserting a \\ so the \ appears correctly in the string
 print('what if I want \\news?')
